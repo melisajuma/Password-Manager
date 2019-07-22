b3ec91cbecb6274e35cf588f3a696b3b06e1adfa
melisajuma <Melisaakinyi95@gmail.com>
2019-07-22 11:24:36 +0300

    writing a method that will check if user details exist when logging in

--- user.py
+++ user.py
@@ -3,14 +3,14 @@ class User:
     The User class will contain user details
     '''
 
-    def __init__(self, login, password):>
+    def __init__(self, login, password):
         '''
         This will ensure the user class has unique properties
         '''
         self.login = login
         self.password = password
 
-    def user_exist(self, password):
+    def user_exist(self, password):>
         '''
         This will first verify that the user exists before showing the passwords
         Args:
