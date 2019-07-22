d5bd7897d3d42575479a8a766865ad3b8a5c8d58
melisajuma <Melisaakinyi95@gmail.com>
2019-07-22 11:53:03 +0300

    importing User class to the run.py file

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
-from Password_Locker import Password>
-from user import User
+from Password_Locker import Password
+from user import User>
 import getpass  # to hide the pass
 
 
